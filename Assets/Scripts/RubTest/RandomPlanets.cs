5b7e64ecc1e7936774bd3a9880a3497db9ed40e4
rubb20 <104703633+rubb20@users.noreply.github.com>
2025-02-08 16:42:17 +0100

    MainMenu, 2 animaciones de Juana. Pausa

--- Assets/Scripts/RubTest/RandomPlanets.cs
+++ Assets/Scripts/RubTest/RandomPlanets.cs
@@ -6,26 +6,19 @@ public class RandomPlanets : MonoBehaviour
 {
     public Vector2 topLeft = new Vector2(-960f, 520f);
     public Vector2 bottomRight = new Vector2(960f, -520f);
+    public GameObject[] planets = new GameObject[3];
 
     private void Awake()
     {
-        this.transform.GetChild(0).transform.position = new Vector3(Random.Range(topLeft.x, bottomRight.x), 
-                                                                    Random.Range(topLeft.y, bottomRight.y),
-                                                                    this.transform.GetChild(0).transform.position.z);
-        this.transform.GetChild(1).transform.position = new Vector3(Random.Range(topLeft.x, bottomRight.x),
-                                                            Random.Range(topLeft.y, bottomRight.y),
-                                                            this.transform.GetChild(0).transform.position.z);
-        this.transform.GetChild(2).transform.position = new Vector3(Random.Range(topLeft.x, bottomRight.x),
-                                                            Random.Range(topLeft.y, bottomRight.y),
-                                                            this.transform.GetChild(0).transform.position.z);
-    }
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
+        Vector3 vector = new Vector3(Random.Range(topLeft.x, bottomRight.x), Random.Range(topLeft.y, bottomRight.y), 0f);
+        planets[0].transform.position = vector;
+        Vector3 vector2 = new Vector3(Random.Range(topLeft.x, bottomRight.x), Random.Range(topLeft.y, bottomRight.y), 0f);
+        while (Vector2.Distance(vector2, planets[0].transform.position) < 2f)
+            vector2 = new Vector3(Random.Range(topLeft.x, bottomRight.x), Random.Range(topLeft.y, bottomRight.y), 0f);
+        planets[1].transform.position = vector2;
+        Vector3 vector3 = new Vector3(Random.Range(topLeft.x, bottomRight.x), Random.Range(topLeft.y, bottomRight.y), 0f);
+        while (Vector2.Distance(vector3, planets[0].transform.position) < 2f && Vector2.Distance(vector, planets[1].transform.position) < 2f)
+            vector3 = new Vector3(Random.Range(topLeft.x, bottomRight.x), Random.Range(topLeft.y, bottomRight.y), 0f);
+        planets[2].transform.position = vector3;
     }
 }
